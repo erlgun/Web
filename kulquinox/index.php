aa81817f2062e894bb5c7ce361b538883559406f
Nicolai Grøndahl <nicolaig@broadpark.no>
2011-11-09 19:44:27 +0100

    la inn mappen kulquinox

---
+++ kulquinox/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Velkommen til Kulquinox</title>
+		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+		<link rel="stylesheet" type="text/css" href="./css/styles.css">
+		<link type="image/x-icon" href="./images/topicon.png" rel="shortcut icon">
+	</head>
+	<body>
+		<div id="outerwrapper">
+			<div id="header">
+				<h1>KULQUINOX</h1>
+			</div> <!-- END OF #header -->
+			<div id="innerwrapper">
+				<div id="mainmenu">
+					<a href="./menu/program">Program</a>
+					<a href="./menu/om_festivalen">Om festivalen</a>
+					<a href="./galleri">Galleri</a>
+					<a href="./nyttig_info">Nyttig info</a>
+					<a href="./kontakt">Kontakt</a>
+				</div> <!-- END OF #mainmenu -->
+				<div id="content">
+				</div> <!-- END OF #content -->
+			</div> <!-- END OF #innerwrapper -->
+		</div> <!-- END OF #outerwrapper -->
+		<div id="push"></div>
+		<div id="footer">
+		</div><!-- END OF #footer -->
+	</body>
+</html>
