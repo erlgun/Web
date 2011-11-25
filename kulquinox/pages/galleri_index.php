db740d2c15292a13a5977c683d6342dc29da0a39
Erlend Gunnesen <erlgun88@gmail.com>
2011-11-25 15:34:31 +0100

    lagt til galleri index

---
+++ kulquinox/pages/galleri_index.php
@@ -0,0 +1,15 @@
+<?php
+$current = 'Galleri';
+$folder = '1';
+$gallery = 'main';
+include '../inc/top.inc.php';
+include '../inc/menu.inc.php';
+include '../inc/galleri_menu.inc.php';
+?>
+
+				<p>
+					Dette er festivalens galleri.
+				</p>
+<?php
+include '../inc/bottom.inc.php';
+?>
