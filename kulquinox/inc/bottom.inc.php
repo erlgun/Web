5a96d673656bbdb91addb3b8e95adccee919c433
Nicolai Grøndahl <nicolaig@broadpark.no>
2011-11-27 22:27:46 +0100

    begynt å jobbe på footer

--- kulquinox/inc/bottom.inc.php
+++ kulquinox/inc/bottom.inc.php
@@ -1,8 +1,16 @@
 			</div> <!-- END OF #content -->
 			<div id="footer">
-				<p>
-					Copyright &copy; <?php $date=getdate(); echo $date['year'];?> Kulquinox
-				</p>	
+				<div id="copyright">
+					<p>
+						Copyright &copy; <?php $date=getdate(); echo $date['year'];?> Kulquinox
+					</p>	
+				</div>
+				<div id="validators">
+					<p>
+						<a class="static_link" href="http://validator.w3.org/check?uri=referer">Valider HTML5 med W3C</a> | 
+						<a class="static_link" href="http://jigsaw.w3.org/css-validator/check/referer">Valider CSS med W3C</a>
+					</p>
+				</div>
 			</div><!-- END OF #footer -->
 		</div> <!-- END OF #wrap -->
 	</body>
