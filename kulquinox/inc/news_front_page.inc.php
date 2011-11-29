3abb61ff09615bf8ee1b87f7a675b2fa7f145896
Nicolai Grøndahl <nicolaig@broadpark.no>
2011-11-29 01:34:29 +0100

    lagt inn ny border på boksene, formateringer av fonter, serifer for h-elementer

--- kulquinox/inc/news_front_page.inc.php
+++ kulquinox/inc/news_front_page.inc.php
@@ -1,4 +1,5 @@
-					<h2>Ny Kulquinox.no!</h2>
-					<p class="front_page">
+					<h2>Nyheter:</h2>
+					<h3><a href="">Ny Kulquinox.no!</a></h3>
+					<p>
 						Ved hjelp av studenter fra <a href="http://www.hioa.no/">HiOA</a> har Kulquinox fått nye nettsider. Fremover vil det bli fokusert på brukervennlighet og enklere navigasjon. <a href="">Les mer</a>.
 					</p>
--- kulquinox/pages/program/artister_index.php
+++ kulquinox/pages/program/artister_index.php
@@ -4,8 +4,70 @@ $folder = '2';
 $artist = 'main';
 include '../../inc/top.inc.php';
 include '../../inc/menu.inc.php';
-include '../../inc/artist_menu.inc.php';
 ?>
+
+<div id="poster_page">
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	</div>
+
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+	<div class="event_stub">
+		<h4>Assula</h4>
+		<p><a href="./artister/assula.php">Assula er en rå fyr som spiller skikkelig musikk! Fuckings yeah!</a></p>
+	
+	</div>
+</div>
 
 <?php
 include '../../inc/bottom.inc.php';
