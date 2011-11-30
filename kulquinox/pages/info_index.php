b9bd9d46ab6942cc5506650f81003ede1fe37f69
Erlend Gunnesen <erlgun88@gmail.com>
2011-11-30 03:40:30 +0100

    fikset valideringsproblem

--- kulquinox/pages/info_index.php
+++ kulquinox/pages/info_index.php
@@ -7,14 +7,14 @@ include '../inc/menu.inc.php';
 
 				<h3>Kart og parkering</h3>
 				<p>
-					<img src="http://maps.googleapis.com/maps/api/staticmap?&center=59.890549,10.525014&zoom=15&size=400x400&maptype=roadmap&markers=color:0x7f8d53|59.890549,10.525014&sensor=false" alt="kart som viser hvor festivalen holder til" />
+					<img src="http://maps.googleapis.com/maps/api/staticmap?&amp;center=59.890549,10.525014&amp;zoom=15&amp;size=400x400&amp;maptype=roadmap&amp;markers=color:0x7f8d53|59.890549,10.525014&amp;sensor=false" alt="kart som viser hvor festivalen holder til" />
 				</p>
 				<p>
 					Parkering
 				</p>
-				<h3>Billetter</h3> <A name="Billetter"></a>
+				<h3>Billetter</h3> <a id="Billetter"></a>
 				<p>
-					Billetter kjøpes på forhånd gjennom <a target="_blank" a href="https://www.baerumkulturhus.no">Bærumkulturhus</a>.
+					Billetter kjøpes på forhånd gjennom <a target="_blank" href="https://www.baerumkulturhus.no">Bærumkulturhus</a>.
 					Budstikkakort og A-kort gir 25% rabatt
 	
 
@@ -48,7 +48,7 @@ include '../inc/menu.inc.php';
 					Grasrotandelen er en ordning fra Norsk Tipping, hvor du som registrert spiller kan velge ett lag eller én forening som du ønsker å støtte - din Grasrotmottaker. Vi oppfordrer deg til å støtte oss i Cosmic Wind Cultural Collaborations!
 				</p>
 				<p>
-					Tenk deg at du kan spille favorittspillene dine - Lotto, Tipping og Joker for eksempel - og samtidig gi noe mer til det laget eller den foreningen du selv ønsker å støtte. Dette er nå mulig via Grasrotandelen. Ved spill hos Norsk Tipping vil 5 % av innsatsen gå direkte til ditt lag eller forening (gjelder ikke Extra og Flax). Merk at Grasrotandelen ikke på noen måte går ut over innsatsen eller premien din - du blir ikke belastet noe for å være grasrotgiver. Du trenger Norsk Tipping Spillerkort for å knytte deg til Grasrotandelen. Spillerkort får du kjøpt hos kommisjonær, eller du kan bestille det på <a target="_blank" a href="https://www.Norsk-Tipping.no">www.norsk-tipping.no</a>.
+					Tenk deg at du kan spille favorittspillene dine - Lotto, Tipping og Joker for eksempel - og samtidig gi noe mer til det laget eller den foreningen du selv ønsker å støtte. Dette er nå mulig via Grasrotandelen. Ved spill hos Norsk Tipping vil 5 % av innsatsen gå direkte til ditt lag eller forening (gjelder ikke Extra og Flax). Merk at Grasrotandelen ikke på noen måte går ut over innsatsen eller premien din - du blir ikke belastet noe for å være grasrotgiver. Du trenger Norsk Tipping Spillerkort for å knytte deg til Grasrotandelen. Spillerkort får du kjøpt hos kommisjonær, eller du kan bestille det på <a target="_blank" href="https://www.Norsk-Tipping.no">www.norsk-tipping.no</a>.
 				</p>
 				<p>
 					Vi oppfordrer deg til å knytte deg til ordningen allerede i dag, og du gjør det på en av følgende måter:
@@ -57,7 +57,7 @@ include '../inc/menu.inc.php';
 					1. Hos Kommisjonær: Ta med deg org.nr 991 534 474 og ditt spillerkort til en av Norsk Tippings mange kommisjonærer.
 				</p>
 				<p>
-					Mer informasjon finnes på <a target="_blank" a href="https://www.norsk-tipping.no/grasrotandelen/">Grasrotandelen</a>
+					Mer informasjon finnes på <a target="_blank" href="https://www.norsk-tipping.no/grasrotandelen/">Grasrotandelen</a>
 				</p>
 				<p>
 					Takk for at du støtter oss via Grasrotandelen.
