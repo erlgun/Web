f5e18122bb4fb5ac4a797d35fb5d4853c1a65dbf
Erlend Gunnesen <erlgun88@gmail.com>
2011-11-30 15:21:26 +0100

    Oppdatert artister

--- kulquinox/pages/program/artister/bollywood_bhangra.php
+++ kulquinox/pages/program/artister/bollywood_bhangra.php
@@ -7,9 +7,15 @@ include '../../../inc/menu.inc.php';
 include '../../../inc/artist_menu.inc.php';
 ?>
 
-						<p>
-							MIDLERTIDIG UTILGJENGELIG
-						</p>
+                    	<div id="artist_text">
+							<img src="../../../images/artists/bollywood_bhangra.jpg" alt="Bilde av Bollywod Bhangra" height="200px" />
+							<p>
+								Bollywood Bhangra er et band/dansegruppe fra India.
+								Bollywood Bhangra har modernisert og rocka opp folkedansen Bhangra.
+								på scene er det både dansere og et helt band.
+								dette sørger for at det virkelig kommer til å bli fart i rockefoten.
+							</p>
+						</div>
 					</div>
 <?php
 include '../../../inc/bottom.inc.php';
--- kulquinox/pages/program/artister/run_dog_run.php
+++ kulquinox/pages/program/artister/run_dog_run.php
@@ -7,9 +7,21 @@ include '../../../inc/menu.inc.php';
 include '../../../inc/artist_menu.inc.php';
 ?>
 
-						<p>
-							MIDLERTIDIG UTILGJENGELIG
-						</p>
+						<div id="artist_text">
+							<img src="../../../images/artists/run_dog_run.jpg" alt="Bilde av artisten Run dog run" height="200px" />
+							<p>
+						          Run Dog Run var et norsk band som startet i 1991
+								I 1993 fikk de platekontrakt med Voices of Wonder. Etter debutalbumet A     Howling Success turnerte de Norge.
+								I 1994 kom EP-en Beauty school Dropout. Run Dog Run fulgte opp med turne i Norge, Danmark, Østerrike og Tyskland.
+							</p> 	 
+							<p>Rundt 1994 og 1995 var Run Dog Run mest aktive, og alle medlemmene jobbet nærmest på heltid med å skrive, arrangere og spille inn sporene som skulle utgjøre albumet Masterplan. Masterplan blei gitt  høsten 1995 . Singelen «Capital» ble spilt endel på P3 sommeren 1995, og deler av låten ble brukt i TV-programmet Lille Lørdag. Masterplan fikk gode anmeldelser, og ble også sluppet i Japan med to bonusspor og tekstene trykket i coveret.
+								Run Dog Run ble oppløst tidlig i 1996 da medlemmene mistet inspirasjonen og ønsket å gjøre andre ting.</p>
+							<p>	
+								Fra 2011 er Run Dog Run igjen aktive.  i 
+						
+							</p>
+							
+						</div>
 					</div>
 <?php
 include '../../../inc/bottom.inc.php';
