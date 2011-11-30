02f271e733f44fef0a49f39a4211986ac9fd989e
Nicolai Grøndahl <nicolaig@broadpark.no>
2011-11-30 03:55:01 +0100

    og litt til på tampen av kvelden

--- kulquinox/inc/programadditions_front_page.php
+++ kulquinox/inc/programadditions_front_page.php
@@ -1,9 +1,9 @@
 					<h2>Siste bookinger:</h2>
-					<h3>Kong Sverre</h3>
+					<h3><a href="./pages/program/artister/kong_sverre.php">Kong Sverre</a></h3>
 					<p class="front_page">
 						Det er med stor glede at vi nok en gang kan skilte med Kulquinox' gamle traver Kong Sverre. Som alltid stiller han opp med sin elektroniske transeuropeiske popmusikk fritt for fuzzgitarer. <a href="./pages/program/artister/kong_sverre.php">Les mer</a>.
 					</p>
-					<h3>Lür</h3>
+					<h3><a href="http://localhost/~nico/Web/kulquinox/pages/program/artister/lur.php">Lür</a></h3>
 					<p class="front_page">
-						De gale svenskene Lür skuffer sjelden med sin transcendale psykedeliske syretripp av et show. Vi er stolte av å ha dem på vår festival, hele crewet gleder seg og det bør også du! <a href="">Les mer</a>.
+						De gale svenskene Lür skuffer sjelden med sin transcendale psykedeliske syretripp av et show. Vi er stolte av å ha dem på vår festival, hele crewet gleder seg og det bør også du! <a href="http://localhost/~nico/Web/kulquinox/pages/program/artister/lur.php">Les mer</a>.
 					</p>
