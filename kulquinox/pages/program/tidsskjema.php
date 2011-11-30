3adb1bd58824a3ca5a499c67b3d460d18e4c2d72
Erlend Gunnesen <erlgun88@gmail.com>
2011-11-30 18:49:13 +0100

    Ordnet pa links til tidsskjema

--- kulquinox/pages/program/tidsskjema.php
+++ kulquinox/pages/program/tidsskjema.php
@@ -7,6 +7,7 @@ include '../../inc/menu.inc.php';
 <h2>Tidsskjema</h2>
 
 		<table class = "schedule">
+				
 				<tr>
 					<th>Tid</th> 
 					<th>Bakgårdsscenen</th>
@@ -14,47 +15,50 @@ include '../../inc/menu.inc.php';
 					<th>Musikkflekken</th>
 				</tr>
 				<tr>
-					<td>21.45 - 23.00</td>
-					<td>Assula</td>
-					<td>Tuva & Tariq</td>
-					<td>Astro-Gastro-JAM</td>
-				</tr>	
-				<tr>
-					<td>20.00 - 21.30</td>
+					<td>13.00 - 13.30</td>
 					<td></td>
-					<td>Seville</td>
-					<td>Lür</td>
+								<div class = "table_links">
+								<td><a href="./artister/bollywood_bhangra.php">Bollywood Bangra</a></td>
+								</div>
+					<td></td>
 				</tr>
 				<tr>
-					<td>19.00 - 19.30</td>
-					<td>Dj Ivaylo</td>
+					<td>14.00 - 14.45</td>
+					<td><a href="./artister/jon_schau.php">Jon Schau</a></td>
 					<td></td>
-					<td>Haraldur</td>
-				</tr>						
+					<td><a href="./artister/radio_9.php">Radio 9</a></td>
+				</tr>
+				<tr>
+					<td>15.00 - 16.00</td>
+					<td><a href="./artister/run_dog_run.php">Run Dog Run</a></td>
+					<td><a href="./artister/khanstrings.php">Khanstrings</a></td>
+					<td></td>
+				</tr>
 				<tr>
 					<td>17.30 - 18.15</td>
 					<td></td>
-					<td>Kong Sverre</td>
+					<td><a href="./artister/kong_sverre.php">Kong Sverre</a></td>
 					<td></td>
-				</tr>			
+				</tr>	
 				<tr>
-					<td>15.00 - 16.00</td>
-					<td>Run Dog Run</td>
-					<td>Khanstrings</td>
+					<td>19.00 - 19.30</td>
+					<td><a href="./artister/dj_ivaylo.php">Dj Ivaylo</a></td>
 					<td></td>
-				</tr>			
-				<tr>
-					<td>14.00 - 14.45</td>
-					<td>Jon Schau</td>
-					<td></td>
-					<td>Radio 9</td>
+					<td><a href="./artister/haraldur.php">Haraldur</a></td>
 				</tr>
 				<tr>
-					<td>13.00 - 13.30</td>
+					<td>20.00 - 21.30</td>
 					<td></td>
-					<td>Bollywood Bangra</td>
-					<td></td>
-				</tr>					
+					<td><a href="./artister/seville.php">Seville</a></td>
+					<td><a href="./artister/lur.php">Lür</a></td>
+				</tr>				
+				<tr>
+					<td>21.45 - 23.00</td>
+					<td><a href="./artister/assula.php">Assula</a></td>
+					<td><a href="./artister/tuva_tariq.php">Tuva & Tariq</a></td>
+					<td><a href="./artister/astro-gastro-jam.php">Astro-Gastro-JAM</a></td>
+				</tr>	
+
 		</table>
 		
 <?php
