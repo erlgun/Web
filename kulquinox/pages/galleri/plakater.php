4fd8db5c403bd7fca0c7b30c3ca4ea5bda00f1c2
Mathias Groven <mathias@nenett.no>
2011-12-01 16:30:41 +0100

    endret alt tekst på bildene

--- kulquinox/pages/galleri/plakater.php
+++ kulquinox/pages/galleri/plakater.php
@@ -8,16 +8,16 @@ include '../../inc/galleri_menu.inc.php';
 ?>
 				
 					<div class="imagelarge">
-						<a target="_blank" href="../../images/gallery/plakat2011.jpg"><img src="../../images/gallery/plakat2011.jpg" alt="Konsertbilde" /></a>
+						<a target="_blank" href="../../images/gallery/plakat2011.jpg"><img src="../../images/gallery/plakat2011.jpg" alt="Kulquinox plakat høst 2011" /></a>
 					</div>
 					<div class="imagelarge">
-						<a target="_blank" href="../../images/gallery/plakat2010.jpg"><img src="../../images/gallery/plakat2010.jpg" alt="Konsertbilde" /></a>
+						<a target="_blank" href="../../images/gallery/plakat2010.jpg"><img src="../../images/gallery/plakat2010.jpg" alt="Kulquinox plakat høst 2010" /></a>
 					</div>
 					<div class="imagelarge">
-						<a target="_blank" href="../../images/gallery/plakat20091.jpg"><img src="../../images/gallery/plakat20091.jpg" alt="Konsertbilde" /></a>
+						<a target="_blank" href="../../images/gallery/plakat20091.jpg"><img src="../../images/gallery/plakat20091.jpg" alt="Kulquinox plakat vår 2010" /></a>
 					</div>
 					<div class="imagelarge">
-						<a target="_blank" href="../../images/gallery/plakat20092.jpg"><img src="../../images/gallery/plakat20092.jpg" alt="Konsertbilde" /></a>
+						<a target="_blank" href="../../images/gallery/plakat20092.jpg"><img src="../../images/gallery/plakat20092.jpg" alt="Kulquinox plakat 2009" /></a>
 					</div>
 				</div> <!-- END OF #inner_content -->					
 <?php
