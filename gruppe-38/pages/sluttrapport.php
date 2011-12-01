98df01eba3815f4bf696a97d09a467b9de57aedb
Nicolai Grøndahl <nicolaig@broadpark.no>
2011-12-01 22:17:38 +0100

    lagt til strukturen av innholdsfortegnelsen til sluttrapporten, her er det bare å fylle på:-)..... omsider ;-)

---
+++ gruppe-38/pages/sluttrapport.php
@@ -0,0 +1,27 @@
+<?php
+$current = 'Sluttrapport';
+include '../inc/headerup.inc.php';
+include '../inc/sidebar.inc.php';
+?>
+
+<h2>Sluttrapport</h2>
+<h3>Innhold</h3>
+<table class="report_menu">
+	<tr>
+		<td>1</td><td><a href="">Innledning</a></td>
+	</tr>
+	<tr>
+		<td>2</td><td><a href="">Hoveddel</a></td>
+	</tr>
+	<tr>
+		<td>3</td><td><a href="">Avslutningsdel</a></td>
+	</tr>
+	<tr>
+		<td>4</td><td><a href=""></a></td>
+	</tr>
+</table>
+
+
+<?php
+include '../inc/footer.inc.php';
+?>
